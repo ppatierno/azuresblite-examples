9a7c9662598dd534f446d335ed9796551bf57df0
Paolo Patierno <ppatierno@live.com>
2015-05-12 13:22:11 +0200

    Added project to read that from event hub

---
+++ IoTEventHubProcessor/IoTEventHubProcessor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSBLite.Examples
+{
+    public class IoTEventHubProcessor : IEventProcessor
+    {
+        private Stopwatch checkpointStopWatch;
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine(string.Format("Processor close.  Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason.ToString()));
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine(string.Format("Processor open.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset));
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (EventData eventData in messages)
+            {
+                if (eventData.Properties["time"] != null)
+                {
+                    Console.WriteLine(string.Format("Received on partition {0} time = {1} body = {2}", context.Lease.PartitionId, eventData.Properties["time"], Encoding.UTF8.GetString(eventData.GetBytes())));
+                }
+            }
+
+            if (this.checkpointStopWatch.Elapsed > TimeSpan.FromSeconds(30))
+            {
+                await context.CheckpointAsync();
+                this.checkpointStopWatch.Restart();
+            }
+        }
+    }
+}
---
+++ IoTEventHubProcessor/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSBLite.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            string eventHubName = ConfigurationManager.AppSettings["EventHubName"];
+            string storageConnectionString = ConfigurationManager.AppSettings["Microsoft.WindowsAzure.Storage.ConnectionString"];
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            eventProcessorHost.RegisterEventProcessorAsync<IoTEventHubProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+        }
+    }
+}
